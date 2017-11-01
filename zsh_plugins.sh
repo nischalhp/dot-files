b55ad438958b62321378139bda2e288281178a8a
Nischal <nischal.hp@gmail.com>
2017-11-01 12:20:12 +0100

    Adding zsh plugins bash script

---
+++ zsh_plugins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+cd ~/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
